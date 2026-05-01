62ec95077c984f869e1aea5e7aa741a31fa43b0c
Alex Nyambura <lxmwaniky@gmail.com>
2026-05-01 05:09:41 +0300

    feat: rename application ID and update MainActivity package structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bike_buddy"
+    namespace = "com.bikebuddy.mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bike_buddy"
+        applicationId = "com.bikebuddy.mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
