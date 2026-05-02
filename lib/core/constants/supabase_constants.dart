231ee25dc07392d4a8d5f392f354bd204090af10
Rodgers-Abraham <abrahamrodgers2@gmail.com>
2026-05-02 19:54:12 +0300

    feat: set up Supabase project with dotenv configuration and update .gitignore

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class SupabaseConstants {
-  // Replace these with your actual Supabase project values
-  static const String url = 'https://enyjegbgwecicylirlgq.supabase.co';
-  static const String anonKey = 'sb_publishable_YmoE_mVCgBINbnRvl-DxWg_DUNyipQs';
+  static String get url => dotenv.env['SUPABASE_URL']!;
+  static String get anonKey => dotenv.env['SUPABASE_ANON_KEY']!;
 }
